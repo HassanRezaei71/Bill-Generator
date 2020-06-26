e40d601c0a0efb12777cf2f85b34ec27d9f455fd
Hassan Rezaei <hassanrezaei1400@gmail.com>
2020-06-27 00:45:47 +0430

    Add Bill Items With Calculate Total Value

--- script/script.js
+++ script/script.js
@@ -4,61 +4,78 @@ const foods = [
     name: "کباب سلطانی",
     price: 117000,
     img: "./img/soltani.jpg",
-    alt: "soltani"
+    alt: "soltani",
+    quantity: 0,
+    total: 0,
   },
   {
     id: 120,
     name: "پیتزا مخصوص",
     price: 22400,
     img: "./img/makhsos.jpg",
-    alt: "makhsos"
+    alt: "makhsos",
+    quantity: 0,
+    total: 0,
   },
   {
     id: 130,
     name: "کوفته",
     price: 35000,
     img: "./img/kofteh.jpg",
-    alt: "kofteh"
+    alt: "kofteh",
+    quantity: 0,
+    total: 0,
   },
   {
     id: 140,
     name: "قورمه سبزی",
     price: 37000,
     img: "./img/ghormeh.jpg",
-    alt: "ghormeh"
+    alt: "ghormeh",
+    quantity: 0,
+    total: 0,
   },
   {
     id: 150,
     name: "باقالی پلو",
     price: 17000,
     img: "./img/baghali.jpg",
-    alt: "baghali"
+    alt: "baghali",
+    quantity: 0,
+    total: 0,
   },
   {
     id: 160,
     name: "فسنجان",
     price: 35000,
     img: "./img/fesenjan.jpg",
-    alt: "fesenjan"
+    alt: "fesenjan",
+    quantity: 0,
+    total: 0,
   },
   {
     id: 170,
     name: "ماهیچه گوسفندی",
     price: 125000,
     img: "./img/mahiche.jpg",
-    alt: "mahiche"
+    alt: "mahiche",
+    quantity: 0,
+    total: 0,
   },
   {
     id: 180,
     name: "ماهی قزل آلا",
     price: 31000,
     img: "./img/ghezel.jpg",
-    alt: "ghezel"
+    alt: "ghezel",
+    quantity: 0,
+    total: 0,
   },
 ];
 
 let cart = {
-  items: [],
+  service: 20500,
+  discount: 34000,
   totalValue: 0,
 };
 
@@ -82,21 +99,103 @@ function renderAllFood() {
         </div>
         <div class="count-container d-flex">
           <div id="count">
-            <span>0</span>
+            <span id="quantity">${food.quantity}</span>
           </div>
           <div class="plus-minus d-flex flex-column">
-            <button id="plus"><i class="fas fa-plus"></i></button>
-            <button id="minus"><i class="fa fa-minus"></i></button>
+            <button id="plus" onclick="addToCart(${
+              food.id
+            })"><i class="fas fa-plus"></i></button>
+            <button id="minus" onclick="removeForCart(${
+              food.id
+            })"><i class="fa fa-minus"></i></button>
           </div>
         </div>
       </div>
     </div>
 
     <div class="price-container">
-      <div class="price">0 تومان</div>
+      <div id="price">${numberWithCommas(food.total)} تومان</div>
     </div>
   </div>`);
   });
 }
 
 renderAllFood();
+
+function renderBillItems() {
+  const bills = $("#bills");
+  const order = $("#order");
+
+  bills.html(`<div class="sum-container d-flex justify-content-between my-1">
+  <div>جمع کل سفارشات</div>
+  <div id="sum">${numberWithCommas(cart.totalValue)} تومان</div>
+  </div>
+  <div
+    class="services-container d-flex justify-content-between my-1"
+  >
+    <div>حق سرویس و کارمزد</div>
+    <div id="services">${numberWithCommas(cart.service)} تومان</div>
+  </div>
+  <div
+    class="discount-container d-flex justify-content-between my-1"
+  >
+    <div>تخفیف</div>
+    <div id="discount">0 تومان</div>
+  </div>
+  <div class="result-container d-flex justify-content-center">
+    <div id="result"> ${numberWithCommas(cart.totalValue)} تومان</div>
+  </div>`);
+
+  order.html(`<div class="discount-code">
+  <input
+    class="input-discount"
+    type="text"
+    placeholder="کد تخفیف"
+  />
+  <button id="add-discount">
+    <i class="fas fa-plus plus-discount"></i>
+  </button>
+  </div>
+  <div class="submit-container">
+    <button class="sub-btn">ثبت سفارش</button>
+  </div>`);
+}
+renderBillItems();
+
+function addToCart(id) {
+  let sum = 0;
+  let foodIndex = foods.findIndex((item) => {
+    return item.id === id;
+  });
+  foods[foodIndex].quantity += 1;
+  foods[foodIndex].total = foods[foodIndex].quantity * foods[foodIndex].price;
+  foods.forEach((item) => {
+    sum += item.total;
+  });
+  if(sum!==0){
+    cart.totalValue = sum;
+  }
+  console.log(cart.totalValue);
+  
+  renderAllFood();
+  renderBillItems();
+}
+
+function removeForCart(id) {
+  let sum = 0;
+  let foodIndex = foods.findIndex((item) => {
+    return item.id === id;
+  });
+  if (foods[foodIndex].quantity > 0) {
+    foods[foodIndex].quantity -= 1;
+    foods[foodIndex].total = foods[foodIndex].quantity * foods[foodIndex].price;
+    foods.forEach((item)=>{
+      sum+=item.total;
+    })
+  }
+  if(sum!==0){
+    cart.totalValue = sum;
+  }
+  renderAllFood();
+  renderBillItems();
+}
